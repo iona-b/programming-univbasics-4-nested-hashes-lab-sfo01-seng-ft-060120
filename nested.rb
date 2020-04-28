e2fa6bf12e103a8e7f231eaee54e61617e190023
Iona <ionabrabender@gmail.com>
2020-04-28 03:10:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,11 +73,10 @@ def adding_matz
 # }
 # return the entire updated hash
 
-  programmer_hash[:yukihiro_matsumoto => {
+  programmer_hash[:yukihiro_matsumoto] => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
         }
-  ]
 
 	programmer_hash = 
  		{
